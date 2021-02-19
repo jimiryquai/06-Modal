0a7a0e082c093352ef0351d88ad1cb3ea7f6d38b
James Ryan <james.ryan@pathwayforcare.com>
2021-02-19 22:50:17 +0000

    Added Esc keydown event listener

--- script.js
+++ script.js
@@ -20,3 +20,8 @@ btnsShowModal.forEach(btn => {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
