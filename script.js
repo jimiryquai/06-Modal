a2e73d310f091a595325f5cb6231fdfbf899bf52
James Ryan <james.ryan@pathwayforcare.com>
2021-02-19 21:33:31 +0000

    Added functions to show and close modal

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsShowModal.forEach(btn => {
+  btn.addEventListener('click', showModal);
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
